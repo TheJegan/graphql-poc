0f9954e912866b3334823928433f2b5305717cb9
Jegan Matthews <jematthe@jematthe011579.starbucks.net>
2018-08-15 13:37:13 -0700

    added default path

--- server.js
+++ server.js
@@ -6,15 +6,25 @@ let port = 4123
 // Construct a schema, using GraphQL schema language
 let schema = buildSchema(`
   type Query {
-    hello: String
+    getLebron: lebron
+  }
+
+  type lebron{
+    name: String
+    team: String
   }
 `);
 
+
+
+
+
 // The root provides a resolver function for each API endpoint
 let root = {
-  hello: () => {
-    return 'Hello world!';
-  },
+  getLebron: {
+    title: "reasons why lebron is better than jordan",
+    credibleNewsSource: "https://www.forbes.com/sites/leighsteinberg/2018/05/31/5-reasons-why-lebron-is-the-greatest-of-all-time/"
+  }
 };
 
 let app = express();
@@ -23,5 +33,10 @@ app.use('/graphql', graphqlHTTP({
   rootValue: root,
   graphiql: true,
 }));
+
+
+app.use('/', function(req,res,next){
+	res.send('server running');
+});
 app.listen(port);
 console.log(`Running a GraphQL API server at localhost:${port}/graphql`);
